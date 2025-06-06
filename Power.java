16969abc223f5895c0f12268e956f64fb12667f6
kavyabasivigalla <basivigallakavya@gmail.com>
2025-06-06 15:16:12 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class Power
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in); 
+        int a = sc.nextInt();   
+        double b = Math.pow(a,3);
+        System.out.println(b);
+  }
+}
