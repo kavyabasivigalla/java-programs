104f9510a07f96abeea235c6b39d5394d7e49f2c
kavyabasivigalla <basivigallakavya@gmail.com>
2025-06-06 15:09:41 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Average
+{
+    public static void main(String[] args)
+    {
+        int a = sc.nextInt();   
+        int b = sc.nextInt();  
+        int c = sc.nextInt();  
+        int d = (a+b+c)/3;
+        System.out.println(d);
+  }
+}
