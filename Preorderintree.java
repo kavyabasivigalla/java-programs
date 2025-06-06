b5dafcf8137cbbc3ed8ae730a041c0bec523ce40
kavyabasivigalla <basivigallakavya@gmail.com>
2025-06-06 15:16:29 +0530

    Add files via upload

---
+++ Preorderintree.java
@@ -0,0 +1,85 @@
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    Node(int data)
+    {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+class tree{
+    Node root;
+    tree()
+    {
+        this.root = null;
+    }
+    void insert(int n)
+    {
+        Node newnode = new Node(n);
+        if (this.root == null)
+        {
+        this.root =  newnode;
+        }
+        else
+        {
+            Node curr = this.root;
+            while(true)
+            {
+                if(n > curr.data)
+                {
+                    if (curr.right == null)
+                    {
+                        curr.right = newnode;
+                        break;
+                    }
+                    else{
+                        curr = curr.right;
+                    }
+                }
+                else 
+                {
+                    if (curr.left == null)
+                    {
+                        curr.left = newnode;
+                        break;
+                    }
+                    else{
+                        curr = curr.left;
+                    }
+                }
+            }
+        }
+    }
+    void inorder(Node root)
+    {
+        if(root != null)
+        {
+            inorder(root.left);
+            System.out.println(root.data);
+            inorder(root.right);
+        }
+    }
+    void preorder(Node root)
+    {
+        if(root != null)
+        {
+            System.out.print(root.data);
+            inorder(root.left);
+            inorder(root.right);
+        }
+    }
+}
+public class Preorderintree
+{
+    public static void main(String[] args)
+    {
+        tree t = new tree();
+        t.insert(3);
+        t.insert(5);
+        t.insert(1);
+        t.postorder(t.root);
+    }
+}
