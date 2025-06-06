d85c24471a7722eae8e37f0e8bc78c482d59519c
kavyabasivigalla <basivigallakavya@gmail.com>
2025-06-06 15:11:07 +0530

    Add files via upload

---
+++ countinll.java
@@ -0,0 +1,63 @@
+class Node 
+{
+    int data;
+    Node next;
+    Node (int n)
+    {
+        this.data=n;
+        this.next=null;
+    }
+}
+class linkedlist
+{
+    Node head = null;
+    void add (int n)
+    {
+        Node newnode = new Node(n);
+        if(head == null)
+        {
+            head = newnode;
+        }
+        else
+        {
+            Node curr = head;
+            while(curr.next != null)
+            {
+                curr = curr.next;
+            }
+            curr.next = newnode;
+        }
+    }
+    void count()
+    {
+        Node curr = this.head;
+        int count = 0;
+        while(curr != null)
+        {
+            count++;
+            curr = curr.next;
+        }
+         System.out.println(count);
+    }
+    void display()
+    {
+        Node curr = head;
+        while(curr != null)
+        {
+           System.out.println(curr.data);
+           curr = curr.next;
+        }
+    }
+}
+public class Countinll
+{
+	public static void main(String[] args)
+	{
+	    linkedlist l = new linkedlist();
+	    l.add(1);
+	    l.add(2);
+	    l.add(3);
+	    l.count();
+	    l.display();
+	}
+}
